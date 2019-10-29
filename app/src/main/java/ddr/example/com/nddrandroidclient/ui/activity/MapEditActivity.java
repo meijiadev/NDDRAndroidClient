d86e9ba7bb1968ed1679ed2e047f630bdcaee828
Ezreal-mei <meijia1219>
2019-10-29 09:48:09 +0800

    新增地图编辑页面+地图采集页面

---
+++ app/src/main/java/ddr/example/com/nddrandroidclient/ui/activity/MapEditActivity.java
@@ -0,0 +1,24 @@
+package ddr.example.com.nddrandroidclient.ui.activity;
+
+import ddr.example.com.nddrandroidclient.common.DDRActivity;
+
+/**
+ * time: 2019/10/29
+ * desc: 地图编辑页面
+ */
+public class MapEditActivity extends DDRActivity {
+    @Override
+    protected int getLayoutId() {
+        return super.getLayoutId();
+    }
+
+    @Override
+    protected void initView() {
+        super.initView();
+    }
+
+    @Override
+    protected void initData() {
+        super.initData();
+    }
+}
