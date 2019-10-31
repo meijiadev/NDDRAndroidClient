89a0d7f933396a04f0630284c85ff9119db14b9f
Ezreal-mei <meijia1219>
2019-10-31 20:58:29 +0800

    给伸缩加上动画效果

--- app/src/main/java/ddr/example/com/nddrandroidclient/ui/fragment/StatusFragment.java
+++ app/src/main/java/ddr/example/com/nddrandroidclient/ui/fragment/StatusFragment.java
@@ -1,9 +1,15 @@
 package ddr.example.com.nddrandroidclient.ui.fragment;
 
 import android.graphics.Color;
+import android.view.View;
 import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import butterknife.BindView;
+import butterknife.OnClick;
 import ddr.example.com.nddrandroidclient.R;
 import ddr.example.com.nddrandroidclient.common.DDRLazyFragment;
 import ddr.example.com.nddrandroidclient.other.Logger;
@@ -15,11 +21,20 @@ import ddr.example.com.nddrandroidclient.widget.StatusSwitchButton;
  * time: 2019/10/26
  * desc: 基础状态界面
  */
-public final class StatusFragment extends DDRLazyFragment<HomeActivity> implements Animation.AnimationListener ,StatusSwitchButton.OnStatusSwitchListener {
+public final class StatusFragment extends DDRLazyFragment<HomeActivity>implements StatusSwitchButton.OnStatusSwitchListener,Animation.AnimationListener{
     @BindView(R.id.status_switch_bt)
     StatusSwitchButton statusSwitchButton;
     @BindView(R.id.circle)
     CircleBarView circleBarView;
+    @BindView(R.id.iv_shrink)
+    ImageView ivShrink;      //点击头部伸缩
+    @BindView(R.id.shrink_tail_layout)
+    LinearLayout shrinkTailLayout; // 伸缩的尾部布局
+    @BindView(R.id.shrink_layout)
+    RelativeLayout shrinkLayout;
+
+    private Animation hideAnimation;  //布局隐藏时的动画
+    private Animation showAnimation;  // 布局显示时的动画效果
 
     public static StatusFragment newInstance(){
         return new StatusFragment();
@@ -33,6 +48,8 @@ public final class StatusFragment extends DDRLazyFragment<HomeActivity> implemen
     @Override
     protected void initView() {
         statusSwitchButton.setOnStatusListener(this);
+        hideAnimation=AnimationUtils.loadAnimation(getAttachActivity(),R.anim.view_hide);
+        showAnimation=AnimationUtils.loadAnimation(getAttachActivity(),R.anim.view_show);
     }
 
     @Override
@@ -41,38 +58,21 @@ public final class StatusFragment extends DDRLazyFragment<HomeActivity> implemen
 
     }
 
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Logger.e("------onRestart");
+    @OnClick({R.id.iv_shrink})
+    public void onViewClicked(View view) {
+        switch (view.getId()){
+            case R.id.iv_shrink:
+                if (shrinkTailLayout.getVisibility()==View.VISIBLE){
+                    shrinkLayout.startAnimation(hideAnimation);
+                    hideAnimation.setAnimationListener(this);
+                }else if (shrinkTailLayout.getVisibility()==View.GONE){
+                    shrinkTailLayout.setVisibility(View.VISIBLE);
+                    shrinkLayout.startAnimation(showAnimation);
+                }
+                break;
+        }
     }
 
-    @Override
-    public void onResume() {
-        super.onResume();
-        Logger.e("-----onResume");
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        Logger.e("------onPause");
-    }
-
-    @Override
-    public void onAnimationStart(Animation animation) {
-
-    }
-
-    @Override
-    public void onAnimationEnd(Animation animation) {
-
-    }
-
-    @Override
-    public void onAnimationRepeat(Animation animation) {
-
-    }
 
     @Override
     public void onLeftClick() {
@@ -86,6 +86,22 @@ public final class StatusFragment extends DDRLazyFragment<HomeActivity> implemen
 
     @Override
     public void onRightClick() {
+
+    }
+
+    @Override
+    public void onAnimationStart(Animation animation) {
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animation animation) {
+        shrinkTailLayout.setVisibility(View.GONE);
+        Logger.e("----动画效果结束");
+    }
+
+    @Override
+    public void onAnimationRepeat(Animation animation) {
 
     }
 }
