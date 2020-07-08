a658cc12d97054042b6b8be58b902289c5efe131
Ezreal-mei <meijia1219>
2020-07-08 11:08:47 +0800

    删除OpenCv库

--- app/src/main/java/ddr/example/com/nddrandroidclient/http/MyObserver.java
+++ app/src/main/java/ddr/example/com/nddrandroidclient/http/MyObserver.java
@@ -7,6 +7,7 @@ import io.reactivex.disposables.Disposable;
  * 监听
  */
 public class MyObserver implements Observer<VersionInformation> {
+
     @Override
     public void onSubscribe(Disposable d) {
 
