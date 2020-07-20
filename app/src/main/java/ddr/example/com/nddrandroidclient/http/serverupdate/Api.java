11018cd82f0da577817f8a99bf179436b7e46aa4
Ezreal-mei <meijia1219>
2020-07-20 18:24:26 +0800

    配置默认url

--- app/src/main/java/ddr/example/com/nddrandroidclient/http/serverupdate/Api.java
+++ app/src/main/java/ddr/example/com/nddrandroidclient/http/serverupdate/Api.java
@@ -1,5 +1,8 @@
 package ddr.example.com.nddrandroidclient.http.serverupdate;
 
+/**
+ * desc :配置默认url
+ */
 public interface Api {
      String APP_DEFAULT_DOMAIN="http://192.168.0.95:8081/";         //默认的URL
      String APP_UPDATE_DOMAIN="http://3g.163.com/";                               //升级app的url
